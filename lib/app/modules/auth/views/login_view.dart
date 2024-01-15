d66c47635da30b1348627a5011fb0ccd16120deb
EngrSagorHossain <sagorhossain4217@gmail.com>
2024-01-15 15:09:24 +0600

    login logic added

--- lib/app/modules/auth/views/login_view.dart
+++ lib/app/modules/auth/views/login_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
 import 'package:huawei_new/app/modules/auth/controllers/auth_controller.dart';
 import 'package:huawei_new/app/modules/auth/views/server_view.dart';
+import 'package:loading_overlay/loading_overlay.dart';
 
 class LoginView extends GetView {
   LoginView({Key? key}) : super(key: key);
@@ -12,150 +14,163 @@ class LoginView extends GetView {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Stack(
-          children: [
-            Positioned(
-                top: 45,
-                right: 0,
-                child: IconButton(
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: InkWell(
-                                onTap: () {
-                                  Get.to(const ServerView());
-                                },
-                                child: const Text('Server')),
-                          );
-                        },
-                      );
-                    },
-                    icon: const Icon(Icons.settings))),
-            SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Align(
-                  //     alignment: Alignment.topRight,
-                  //     child: Icon(Icons.ac_unit,color: Colors.red,size: 25,)),
-                  Container(
-                    height: 530,
-                    width: 250,
-                    decoration: BoxDecoration(
-                        border: Border.all(width: 1, color: Colors.black)),
-                    child: Column(
-                      children: [
-                        Image.asset(
-                          "images/default/huawei.png",
-                          width: 250,
-                        ),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        const Text(
-                          "WELCOME",
-                          style: TextStyle(color: Colors.grey, fontSize: 25),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: TextField(
-                            controller: _userNameController,
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              hintText: 'UserName',
-                              hintStyle: TextStyle(color: Colors.black),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: TextField(
-                            controller: _passWordController,
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              hintText: 'Password',
-                              hintStyle: TextStyle(color: Colors.black),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.blue),
-                            onPressed: () {
-                              // Get.to(const BetView());
-                              authController.tryToSignIn(
-                                userName: _userNameController.text,
-                                password: _passWordController.text,
+      body: Obx(() {
+        return LoadingOverlay(
+          isLoading: authController.authLoading.value,
+          progressIndicator: const SpinKitDoubleBounce(
+            color: Colors.blueAccent,
+            size: 60,
+          ),
+          child: Center(
+            child: Stack(
+              children: [
+                Positioned(
+                    top: 45,
+                    right: 0,
+                    child: IconButton(
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: InkWell(
+                                    onTap: () {
+                                      Get.to(const ServerView());
+                                    },
+                                    child: const Text('Server')),
                               );
                             },
-                            child: const Center(
-                                child: Text("Login",
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 14))),
-                          ),
-                        ),
-                        const Column(
+                          );
+                        },
+                        icon: const Icon(Icons.settings))),
+                SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Align(
+                      //     alignment: Alignment.topRight,
+                      //     child: Icon(Icons.ac_unit,color: Colors.red,size: 25,)),
+                      Container(
+                        height: 530,
+                        width: 250,
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 1, color: Colors.black)),
+                        child: Column(
                           children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text("English",
-                                    style: TextStyle(
-                                        color: Colors.blue,
-                                        fontSize: 13,
-                                        fontWeight: FontWeight.bold)),
-                                Text("|", style: TextStyle(color: Colors.grey)),
-                                Text("Eng",
-                                    style: TextStyle(color: Colors.blue)),
-                              ],
+                            Image.asset(
+                              "images/default/huawei.png",
+                              width: 250,
                             ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            const Text(
+                              "WELCOME",
+                              style:
+                                  TextStyle(color: Colors.grey, fontSize: 25),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextField(
+                                controller: _userNameController,
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'UserName',
+                                  hintStyle: TextStyle(color: Colors.black),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextField(
+                                controller: _passWordController,
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Password',
+                                  hintStyle: TextStyle(color: Colors.black),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.blue),
+                                onPressed: () {
+                                  // Get.to(const BetView());
+                                  authController.tryToSignIn(
+                                    userName: _userNameController.text,
+                                    password: _passWordController.text,
+                                  );
+                                },
+                                child: const Center(
+                                    child: Text("Login",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14))),
+                              ),
+                            ),
+                            const Column(
                               children: [
-                                Text("Version",
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 13,
-                                        fontWeight: FontWeight.bold)),
-                                Text(":", style: TextStyle(color: Colors.grey)),
-                                Text("7.2",
-                                    style: TextStyle(color: Colors.black)),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text("English",
+                                        style: TextStyle(
+                                            color: Colors.blue,
+                                            fontSize: 13,
+                                            fontWeight: FontWeight.bold)),
+                                    Text("|",
+                                        style: TextStyle(color: Colors.grey)),
+                                    Text("Eng",
+                                        style: TextStyle(color: Colors.blue)),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text("Version",
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 13,
+                                            fontWeight: FontWeight.bold)),
+                                    Text(":",
+                                        style: TextStyle(color: Colors.grey)),
+                                    Text("7.2",
+                                        style: TextStyle(color: Colors.black)),
+                                  ],
+                                ),
                               ],
                             ),
                           ],
                         ),
-                      ],
-                    ),
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      const Text(
+                        "Visit Our Website",
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Copyright 2018. All rights reserved",
+                        style: TextStyle(fontSize: 10, color: Colors.black),
+                      )
+                    ],
                   ),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  const Text(
-                    "Visit Our Website",
-                    style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text(
-                    "Copyright 2018. All rights reserved",
-                    style: TextStyle(fontSize: 10, color: Colors.black),
-                  )
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      }),
     );
   }
 }
