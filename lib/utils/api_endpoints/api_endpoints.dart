35d35393ab7e74dba067c102487b544a32677efd
EngrSagorHossain <sagorhossain4217@gmail.com>
2024-08-22 15:17:03 +0600

    splash problem fixed

--- lib/utils/api_endpoints/api_endpoints.dart
+++ lib/utils/api_endpoints/api_endpoints.dart
@@ -1,4 +1,4 @@
-const String kDomain = "https://wlottery.sheikhit.net";
+const String kDomain = "https://malaylotary.com";
 const String basUrl = "$kDomain/api";
 //Authentication EndPoints
 const String kLoginUlr = "$basUrl/login";
@@ -11,8 +11,6 @@ String kCancelOrder({required int orderId}) => "$basUrl/order-cancel/$orderId";
 const String kGetAllOrders = "$basUrl/orders";
 const String kGetAllAcceptedOder = "$basUrl/totalaccept";
 const String kMakeOrder2 = "$basUrl/convert-data";
-
 //winloss
-
 const String kWinLoss = "$basUrl/get-history";
 const String kGetWinNotice = "$basUrl/get_win_note";
